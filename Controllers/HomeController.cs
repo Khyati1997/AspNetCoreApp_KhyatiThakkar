578ba17d5c908e14fa0c6ba305bd8e6e35fc504f
khyati1997 <kthakk28@my.centennialcollege.ca>
2025-01-25 20:52:38 -0500

    Hello World ASPNetCore Application

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using AspNetCoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        // Constructor to initialize the logger
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Main Index action to display a greeting message
+        public IActionResult Index()
+        {
+            // Pass a custom greeting message to the view
+            ViewData["Message"] = "Hello, World!";
+            return View();
+        }
+
+        // Privacy action to show the privacy policy page
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        // Error action to handle errors and display an error page
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
